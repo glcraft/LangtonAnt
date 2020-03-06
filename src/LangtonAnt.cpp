4f2322667bb34a7bd09674743cd8e2442f445cf0
Gabin Lefranc <gabin.lefranc@gmail.com>
2020-03-07 00:30:07 +0100

    Shader code outside of the init function

--- src/LangtonAnt.cpp
+++ src/LangtonAnt.cpp
@@ -1,5 +1,35 @@
 #include <LangtonAnt.h>
 #include <limits>
+
+const std::string_view strVertShad=R"(
+#version 330 core
+layout (location=0) in vec2 pos;
+void main()
+{
+    gl_Position = vec4(pos, 0, 1);
+}
+)";
+const std::string_view strFragShad=R"(
+#version 330 core
+uniform sampler2D tex;
+uniform uvec2 screenSize;
+uniform float coef;
+out vec4 outColor;
+
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+void main()
+{
+    float value=texture(tex, gl_FragCoord.xy/vec2(screenSize)).r;
+    if (value!=0.)
+        outColor=vec4(hsv2rgb(vec3(value*coef,1,1)), 1);
+    else
+        outColor=vec4(0,0,0,1);
+}
+)";
 LangtonAnt::LangtonAnt(uint32_t winWidth, uint32_t winHeight, uint8_t size) : m_size(size), m_winsize(winWidth, winHeight)
 {
     m_total = m_winsize.x * m_winsize.y / (m_size*m_size);
@@ -43,36 +73,8 @@ void LangtonAnt::init()
         gl::sl::Shader<gl::sl::Vertex> vshad;
         gl::sl::Shader<gl::sl::Fragment> fshad;
         try{
-        vshad.set(R"(
-#version 330 core
-layout (location=0) in vec2 pos;
-void main()
-{
-    gl_Position = vec4(pos, 0, 1);
-}
-        )", false);
-        fshad.set(R"(
-#version 330 core
-uniform sampler2D tex;
-uniform uvec2 screenSize;
-uniform float coef;
-out vec4 outColor;
-
-vec3 hsv2rgb(vec3 c) {
-    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
-void main()
-{
-    float value=texture(tex, gl_FragCoord.xy/vec2(screenSize)).r;
-    if (value!=0.)
-        outColor=vec4(hsv2rgb(vec3(value*coef,1,1)), 1);
-    else
-        outColor=vec4(0,0,0,1);
-}
-        )", false);
-        
+            vshad.set(strVertShad.data(), false);
+            fshad.set(strFragShad.data(), false);
             m_shader << vshad << fshad << gl::sl::link;
         }
         catch (gl::sl::CompileException exc)
